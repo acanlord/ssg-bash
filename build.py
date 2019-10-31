6f9fa32b2f49117be4a03c555245a4b363200034
rich <rich@richs-Mac-Pro.local>
2019-10-31 14:50:30 -0700

    udpated build.py

--- build.py
+++ build.py
@@ -1,5 +1,9 @@
 #! /usr/bin/python
 
 
-filenames = ['top.html', 'index.html', 'bottom.html']
-with open(
+
+filenames = ['file1.txt', 'file2.txt', ...]
+with open('path/to/output/file', 'w') as outfile:
+    for fname in filenames:
+        with open(fname) as infile:
+            outfile.write(infile.read())
