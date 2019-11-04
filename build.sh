f98c8c5105510ca433533906d087d2f514b83c10
Richard Houck <rdhouck@gmail.com>
2019-11-04 11:27:22 -0800

    Updated output path

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ rm -rf *.html
 echo "Were goign to gen your new html files"
 
 # Gen new files
-cat templates/top.html content/index.html templates/bottom.html > index.html
-cat templates/top.html content/blog.html templates/bottom.html > blog.html
-cat templates/top.html content/projects.html templates/bottom.html > projects.html
+cat templates/top.html content/index.html templates/bottom.html > ./docs/index.html
+cat templates/top.html content/blog.html templates/bottom.html > ./docs/blog.html
+cat templates/top.html content/projects.html templates/bottom.html > ./docs/projects.html
 
 echo "Build Complete, here's the contents of docs"
-ls -l *.html 
+ls -l ./docs 
 open .
