8d326c99de7fdce41f637d0f4f3c8cbae4679a61
Richard Houck <rdhouck@gmail.com>
2019-11-05 11:04:26 -0800

    udpated bash scriptOCwq

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # Generate new site content
 
 
-# Clean old files
-echo "Removing old files"
-rm -rf ./docs/*.html
-
 echo "Were goign to gen your new html files"
 
 # Gen new files
@@ -15,4 +11,3 @@ cat templates/top.html content/projects.html templates/bottom.html > ./docs/proj
 
 echo "Build Complete, here's the contents of docs"
 ls -l ./docs 
-open .
